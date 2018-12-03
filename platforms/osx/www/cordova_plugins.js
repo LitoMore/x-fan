4cf2e2e218bbb2a31533dcf47a2967c091105af8
LitoMore <LitoMore@users.noreply.github.com>
2018-12-03 12:29:57 +0800

    Add Cordova framework (#8)
    * Add cordova
    
    * Ignore build files
    
    * Ignore build files
    
    * Update build script
    
    * Update cordova build config
    
    * Add cordova-plugin-statusbar
    
    * Add eof
    
    * Add cordova build files
    
    * Add all platform

---
+++ platforms/osx/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
---
+++ platforms/windows/www/precache-manifest.1bbdb691b1753da3fdbce7f8ae23d969.js
@@ -0,0 +1,62 @@
+﻿self.__precacheManifest = [
+  {
+    "revision": "4fda75d2d27b7830fe2d",
+    "url": "/static/css/main.979c41a4.chunk.css"
+  },
+  {
+    "revision": "4fda75d2d27b7830fe2d",
+    "url": "/static/js/main.4fda75d2.chunk.js"
+  },
+  {
+    "revision": "0fd53ae104dbd22051b8",
+    "url": "/static/css/1.dffae40f.chunk.css"
+  },
+  {
+    "revision": "0fd53ae104dbd22051b8",
+    "url": "/static/js/1.0fd53ae1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a5753ed26002645d2a01c371c32df68",
+    "url": "/static/media/github.5a5753ed.svg"
+  },
+  {
+    "revision": "ae8767ca8ecf4b090af58eff5dd6d3a7",
+    "url": "/static/media/Framework7Icons-Regular.ae8767ca.woff2"
+  },
+  {
+    "revision": "2e9a0313ed02aed1e206340feb36ec5c",
+    "url": "/static/media/Framework7Icons-Regular.2e9a0313.woff"
+  },
+  {
+    "revision": "4348368ab857cb57b18e1a87ba8ff284",
+    "url": "/static/media/Framework7Icons-Regular.4348368a.ttf"
+  },
+  {
+    "revision": "c0087d4d5ddd32ec52f1859b42956db6",
+    "url": "/static/media/Framework7Icons-Regular.c0087d4d.eot"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/static/media/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/static/media/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/static/media/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "d41ae58dab60e355ae3872ebf2f1403a",
+    "url": "/static/media/MaterialIcons-Regular.d41ae58d.ttf"
+  },
+  {
+    "revision": "e1a0890b40d55db65b9b9920f1011c87",
+    "url": "/index.html"
+  }
+];
