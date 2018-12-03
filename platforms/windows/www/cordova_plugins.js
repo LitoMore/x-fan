fd995361840a270824834c249f1bda928dedadd8
LitoMore <litomore@gmail.com>
2018-12-03 11:53:10 +0800

    Add all platform

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.StatusBarProxy",
+    "file": "plugins/cordova-plugin-statusbar/src/windows/StatusBarProxy.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "runs": true
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-statusbar": "2.4.2",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
